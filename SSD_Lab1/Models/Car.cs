6c3cd12bc9968cce292642f37315c3b84529cefc
Ryan Harris <ryan689harris@gmail.com>
2025-10-08 19:04:05 -0400

    added models

---
+++ SSD_Lab1/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SSD_Lab1.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Car Model")]
+        [StringLength(100)]
+        public string Model { get; set; }
+
+        [Required]
+        [Display(Name = "Model Year")]
+        [Range(1885, 2135, ErrorMessage = "Model year must be between 1885 and 2135.")]
+        public int ModelYear { get; set; }
+
+        [Required]
+        [Display(Name = "Cost")]
+        public float Cost { get; set; }
+
+    }
+}
---
+++ SSD_Lab1/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SSD_Lab1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(100)]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Year of Birth")]
+        [Range(1820, 2020, ErrorMessage = "Year of birth must be between 1820 and 2020.")]
+        public int ModelYear { get; set; }
+        [Required]
+        [Display(Name = "Wage")]
+        public float Wage { get; set; }
+
+    }
+}
